c0b845e81260c9c3adb02b74a66e0d8929cb6baf
suvel <vdr_Suvelrathneswar@airasia.com>
2023-02-26 18:34:32 +0530

    feat: :art: Converting gray img to asci strings to visualize

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const ascii = require('./src/assets/ascii');
 const processingFrameCount = 2;
 
 async function main() {
+  let buffer = [];
   for (let frame = 0; frame <= processingFrameCount; frame++) {
     const savedFrameFullPath = await video.videoToFrames(frame);
-    ascii.getGrayScaledImg(savedFrameFullPath, frame);
+    buffer.push(savedFrameFullPath);
+  }
+  for (frame = 0; frame <= processingFrameCount; frame++) {
+    try {
+      const storedFrameFPath = buffer[frame];
+      const [imgObj, grayImgFullPath] = await ascii.getGrayScaledImg(
+        storedFrameFPath,
+        frame
+      );
+      debugger;
+      const asciiArtFrame = await ascii.convertImgObj2ASCII(
+        imgObj,
+        grayImgFullPath
+      );
+      console.log(asciiArtFrame);
+    } catch (exe) {
+      console.log(exe);
+    }
   }
 }
 
--- src/assets/ascii.js
+++ src/assets/ascii.js
@@ -1,22 +1,58 @@
 const Jimp = require('jimp');
+const ascii = require('ascii-art');
 
 const defaultOutputPath = `src/assets/output`;
 const outputImgFormate = 'png';
 
 function getGrayScaledImg(inputImage, frameNumber) {
-  console.log(`-> optimizing img in frame ${frameNumber}`);
-  const outputFileName = `${frameNumber}.${outputImgFormate}`;
-  Jimp.read(inputImage, function (err, image) {
-    if (err) throw err;
-    image
-      .resize(100, Jimp.AUTO) // Resize the image to a width of 100 pixels.
-      .grayscale() // Convert the image to grayscale.
-      .write(`${defaultOutputPath}/${outputFileName}`); // Save the grayscale image to disk.
+  return new Promise((resolve, reject) => {
+    console.log(`-> optimizing img in frame ${frameNumber}`);
+    const outputFileName = `${frameNumber}.${outputImgFormate}`;
+    Jimp.read(inputImage, function (err, image) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      image
+        .resize(100, Jimp.AUTO) // Resize the image to a width of 100 pixels.
+        .grayscale() // Convert the image to grayscale.
+        .write(`${defaultOutputPath}/${outputFileName}`)// Save the grayscale image to disk.
+        
+
+      console.log('-/-> Finished processing');
+      console.log(
+        `(i) Frame ${frameNumber} optimized and stored at ${defaultOutputPath}/${outputFileName}`
+      );
+
+      resolve([image, `${defaultOutputPath}/${outputFileName}`]);
+    });
   });
-  console.log('-/-> Finished processing');
-  console.log(
-    `(i) Frame ${frameNumber} optimized and stored at ${defaultOutputPath}/${outputFileName}`
-  );
 }
 
-module.exports = { getGrayScaledImg };
+function convertImgObj2ASCII(imageObj, fileFullPath) {
+  return new Promise((resolve, reject) => {
+    try {
+      ascii.image(
+        {
+          filepath: fileFullPath,
+          width: imageObj.bitmap.width * 0.6,
+          height: imageObj.bitmap.height * 0.6,
+          color: false,
+          stringify: true,
+        },
+        function (err, converted) {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve(converted);
+          }
+        }
+      );
+    } catch (exe) {
+      console.log(exe);
+    }
+  });
+}
+
+module.exports = { getGrayScaledImg, convertImgObj2ASCII };
